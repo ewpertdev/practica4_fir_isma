54e1d5328d5ec915047cc343ffb6c1b0d005ee56
ewpertdev <xblackcat93@gmail.com>
2024-11-07 13:02:50 +0100

    Hash SHA-256

--- src/ej1/Bruteforce.java
+++ src/ej1/Bruteforce.java
@@ -1,5 +1,5 @@
 package ej1;
 
 public class Bruteforce {
-    private String palabra = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
+    private String palabras = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
 }
