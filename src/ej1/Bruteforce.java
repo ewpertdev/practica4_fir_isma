0c1a7127f7e68a92b264fc354a86b5b4e1979d23
ewpertdev <xblackcat93@gmail.com>
2024-11-07 13:17:27 +0100

    Longitud de la palabra

--- src/ej1/Bruteforce.java
+++ src/ej1/Bruteforce.java
@@ -1,5 +1,6 @@
 package ej1;
 
 public class Bruteforce {
-    privae String palabras = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
+    private final static String palabras = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
+    private final static int longitudPalanra = 4;
 }
