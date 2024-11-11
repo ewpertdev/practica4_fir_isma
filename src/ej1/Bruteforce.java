fe6178b24e7a1833f137133cf8f1751bdd86e5a2
ewpertdev <xblackcat93@gmail.com>
2024-11-11 10:07:49 +0100

    Added método obtener hash

--- src/ej1/Bruteforce.java
+++ src/ej1/Bruteforce.java
@@ -1,6 +1,30 @@
 package ej1;
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Bruteforce {
     private final static String palabras = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
-    private final static int longitudPalanra = 4;
+    private final static int longitudPalabra = 4;
+    private static String passwordEncontrada = null;
+
+    private static byte[] obtenerHash(String text) {
+        try {
+            MessageDigest digest = MessageDigest.getInstance("SHA-256");
+            return digest.digest(text.getBytes(StandardCharsets.UTF_8));
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static void main (String[] args) {
+
+
+
+        }
+
+
 }
+
