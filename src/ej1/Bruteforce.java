bd0cde7fdd5788bd144142deddd09fe55d8c78d0
ewpertdev <xblackcat93@gmail.com>
2024-11-11 10:40:26 +0100

    Añadido variable Atomic y número de hilos

--- src/ej1/Bruteforce.java
+++ src/ej1/Bruteforce.java
@@ -3,10 +3,13 @@ package ej1;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Bruteforce {
     private final static String palabras = "b221d9dbb083a7f33428d7c2a3c3198ae925614d70210e28716ccaa7cd4ddb79";
     private final static int longitudPalabra = 4;
+    private static final int numHilos = Runtime.getRuntime().availableProcessors();
+    private static final AtomicBoolean found = new AtomicBoolean(false);
     private static String passwordEncontrada = null;
 
     private static byte[] obtenerHash(String text) {
