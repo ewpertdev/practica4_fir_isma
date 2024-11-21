b6a45630a1acb749315decb0621b1678ccc527c6
ewpertdev <xblackcat93@gmail.com>
2024-11-22 00:03:41 +0100

    feat: Implementación completa de la clase Client
    - Añade constructor completo
    - Implementa getters y setters
    - Añade método sincronizado para actualizar saldo
    - Implementa toString para depuración
    - Añade documentación JavaDoc completa
    
    Implementación de la clase que representa a los clientes
    del sistema bancario con toda su funcionalidad básica.

--- src/ej3/model/Client.java
+++ src/ej3/model/Client.java
@@ -23,5 +23,54 @@ public class Client {
         // Constructor vacío para Jackson
     }
 
-    // Getters y setters básicos - los completaremos en el siguiente commit
+    /**
+     * Constructor con todos los campos.
+     *
+     * @param id Identificador del cliente
+     * @param nombre Nombre del cliente
+     * @param saldo Saldo actual del cliente
+     * @param numeroCuenta Número de cuenta bancaria
+     * @param direccion Dirección del cliente
+     */
+    public Client(String id, String nombre, double saldo, String numeroCuenta, String direccion) {
+        this.id = id;
+        this.nombre = nombre;
+        this.saldo = saldo;
+        this.numeroCuenta = numeroCuenta;
+        this.direccion = direccion;
+    }
+
+    // Getters
+    public String getId() { return id; }
+    public String getNombre() { return nombre; }
+    public double getSaldo() { return saldo; }
+    public String getNumeroCuenta() { return numeroCuenta; }
+    public String getDireccion() { return direccion; }
+
+    // Setters
+    public void setId(String id) { this.id = id; }
+    public void setNombre(String nombre) { this.nombre = nombre; }
+    public void setSaldo(double saldo) { this.saldo = saldo; }
+    public void setNumeroCuenta(String numeroCuenta) { this.numeroCuenta = numeroCuenta; }
+    public void setDireccion(String direccion) { this.direccion = direccion; }
+
+    /**
+     * Actualiza el saldo del cliente de forma sincronizada.
+     * 
+     * @param cantidad Cantidad a añadir (positiva) o restar (negativa)
+     */
+    public synchronized void actualizarSaldo(double cantidad) {
+        this.saldo += cantidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id='" + id + '\'' +
+                ", nombre='" + nombre + '\'' +
+                ", saldo=" + saldo +
+                ", numeroCuenta='" + numeroCuenta + '\'' +
+                ", direccion='" + direccion + '\'' +
+                '}';
+    }
 } 
