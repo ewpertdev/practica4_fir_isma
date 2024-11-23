ed78f5e0ad55298d3ce0fe9678f7cd91122816a3
ismaellozano2 <isma.lozano.dosa@gmail.com>
2024-11-23 19:59:02 +0100

    Añadir imports necesarios

--- src/SimuladorMinasYOro_Ismael_Fir/Principal.java
+++ src/SimuladorMinasYOro_Ismael_Fir/Principal.java
@@ -1,5 +1,8 @@
 package juego;
 
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 public class Principal {
      // Tamaño del terreno y número de jugadores configurables
     private static final int TAMANIO_TERRENO = 8; // Cambiar el tamaño del terreno aquí
