ed889f75671d07773ae0a72f8eafcf5343b84924
ismaellozano2 <isma.lozano.dosa@gmail.com>
2024-11-23 19:52:24 +0100

    Constantes para tamaño del terreno y número de jugadores

--- src/SimuladorMinasYOro_Ismael_Fir/Principal.java
+++ src/SimuladorMinasYOro_Ismael_Fir/Principal.java
@@ -1,6 +1,12 @@
 package juego;
 
 public class Principal {
+     // Tamaño del terreno y número de jugadores configurables
+    private static final int TAMANIO_TERRENO = 8; // Cambiar el tamaño del terreno aquí
+    private static final int NUMERO_JUGADORES = 4; // Cambiar el número de jugadores aquí
+    private static Terreno terreno;
+    private static ExecutorService ejecutor;
+    
     public static void main(String[] args) {
         
     }
