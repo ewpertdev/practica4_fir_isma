13c706c06d3b8451e94a220d2ee93ddc2682468d
ismaellozano2 <isma.lozano.dosa@gmail.com>
2024-11-24 17:20:41 +0100

    Arreglando fallos

--- src/SimuladorMinasYOro_Ismael_Fir/Terreno.java
+++ src/SimuladorMinasYOro_Ismael_Fir/Terreno.java
@@ -1,16 +1,18 @@
-package juego;
+package SimuladorMinasYOro_Ismael_Fir;
 
 import java.util.Random;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class Terreno {
-    private final char[][] cuadrado;    // Matriz que representa el terreno
-    private final int tamanio;         // Tamaño del terreno (siempre cuadrado)
-    private final AtomicBoolean juegoTerminado; // Indica si el juego ha terminado
+    private final char[][] cuadrado;          // Representación del terreno
+    private final int tamanio;               // Tamaño del terreno
+    private final Random aleatorio;          // Generador de posiciones aleatorias
+    private final AtomicBoolean juegoTerminado; // Bandera para indicar si el juego terminó
 
     public Terreno(int tamanio) {
         this.tamanio = tamanio;
         this.cuadrado = new char[tamanio][tamanio];
-        this.juegoTerminado = new AtomicBoolean(false); // Inicialmente no terminado
+        this.aleatorio = new Random();
+        this.juegoTerminado = new AtomicBoolean(false); // Inicializamos el juego como no terminado
     }
 }
