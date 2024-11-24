3f0ee5e278b0be29000ed669695b50c9f74a2287
ismaellozano2 <isma.lozano.dosa@gmail.com>
2024-11-24 17:07:44 +0100

    Crear Terreno con atributos

---
+++ src/SimuladorMinasYOro_Ismael_Fir/Terreno.java
@@ -0,0 +1,15 @@
+package juego;
+
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class Terreno {
+    private final char[][] cuadrado;    // Matriz que representa el terreno
+    private final int tamanio;         // Tamaño del terreno (siempre cuadrado)
+    private final AtomicBoolean juegoTerminado; // Indica si el juego ha terminado
+
+    public Terreno(int tamanio) {
+        this.tamanio = tamanio;
+        this.cuadrado = new char[tamanio][tamanio];
+        this.juegoTerminado = new AtomicBoolean(false); // Inicialmente no terminado
+    }
+}
