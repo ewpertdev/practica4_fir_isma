d5f54efafb3d02249704fd954ade342f5b256761
ismaellozano2 <isma.lozano.dosa@gmail.com>
2024-11-24 18:23:49 +0100

    Añadir logica basica para mover al jugador

--- src/SimuladorMinasYOro_Ismael_Fir/Jugador.java
+++ src/SimuladorMinasYOro_Ismael_Fir/Jugador.java
@@ -11,4 +11,20 @@ public class Jugador implements Runnable {
         this.id = id;
         this.terreno = terreno;
         this.aleatorio = new Random();
+        
     }
+@Override
+    public void run() {
+        // Mientras el juego no haya terminado, el jugador intenta moverse
+        while (!terreno.juegoTerminado()) {
+            int x = aleatorio.nextInt(terreno.getTamanio());
+            int y = aleatorio.nextInt(terreno.getTamanio());
+
+            // Solo intentamos movernos si la posición es válida
+            if (terreno.posicionValida(x, y)) {
+                boolean terminado = terreno.moverJugador(x, y, id); // Movimiento del jugador
+                terreno.imprimirTerreno(); // Imprimimos el estado del terreno después del movimiento
+                if (terminado) {
+                    break; // Salimos si el juego ha terminado
+                }
+            }
