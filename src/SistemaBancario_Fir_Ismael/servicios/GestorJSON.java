491d2217f4e2ebad6c32334a749fbbb620326f34
ewpertdev <xblackcat93@gmail.com>
2024-11-25 16:55:54 +0100

    Corregidas importaciones de Jackson en todas las clases

--- src/SistemaBancario_Fir_Ismael/servicios/GestorJSON.java
+++ src/SistemaBancario_Fir_Ismael/servicios/GestorJSON.java
@@ -1,6 +1,8 @@
 package SistemaBancario_Fir_Ismael.servicios;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.core.type.TypeReference;
 import SistemaBancario_Fir_Ismael.modelo.Cliente;
 import SistemaBancario_Fir_Ismael.modelo.Transferencia;
 import java.io.File;
@@ -8,7 +10,12 @@ import java.io.IOException;
 import java.util.List;
 
 public class GestorJSON {
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper;
+    
+    static {
+        mapper = new ObjectMapper();
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+    }
 
     public static Cliente leerCliente(String rutaArchivo) throws IOException {
         File file = new File(rutaArchivo);
